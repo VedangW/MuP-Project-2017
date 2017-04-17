c3370eef0fdcb93f6ad8627310efcd9e97baf5cc
Vedang Waradpande <f2015028@goa.bits-pilani.ac.in>
2017-04-17 22:44:26 +0530

    Started with 8255

--- fanSimulator.asm
+++ fanSimulator.asm
@@ -3,8 +3,31 @@
 .486
 .data
 
+;8255 data
+	creg	equ	06h
+
 .code
 .startup
+
+;Programming 8255
+init8255_:
+	mov al, 83h
+	out creg, al
+
+kr_10:
+	mov al, 00h
+	out 04h, al
+kr_11:
+	in	al, 04h
+	and al, 0f0h
+	cmp al, 0e0h
+	jnz kr_11
+	call delay_20ms
+	
+	mov al, 00h
+;Programming 8254s
+init8254_1:
+	
 
 .exit
 end
